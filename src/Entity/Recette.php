33a0f8d47fa3b096f900fffc152c65abe3e7d4c0
Shabadine <bah.shabadine@outlook.fr>
2025-10-31 17:12:44 +0100

    feat(Entities): Ajout de l'entité recette

---
+++ src/Entity/Recette.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecetteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecetteRepository::class)]
+class Recette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $temps_prepration = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $difficulte = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $date_creation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTempsPrepration(): ?int
+    {
+        return $this->temps_prepration;
+    }
+
+    public function setTempsPrepration(int $temps_prepration): static
+    {
+        $this->temps_prepration = $temps_prepration;
+
+        return $this;
+    }
+
+    public function getDifficulte(): ?string
+    {
+        return $this->difficulte;
+    }
+
+    public function setDifficulte(string $difficulte): static
+    {
+        $this->difficulte = $difficulte;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeImmutable
+    {
+        return $this->date_creation;
+    }
+
+    public function setDateCreation(\DateTimeImmutable $date_creation): static
+    {
+        $this->date_creation = $date_creation;
+
+        return $this;
+    }
+}

8285cad261979c94643818065a9cd6b65fae10ef
Shabadine <bah.shabadine@outlook.fr>
2025-11-01 12:09:37 +0100

    feat(Security): Sécurisation de Ingredient et Categorie Controllers (ROLE_USER)

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/categorie')]
+#[IsGranted('ROLE_USER')]
 final class CategorieController extends AbstractController
 {
     #[Route(name: 'app_categorie_index', methods: ['GET'])]

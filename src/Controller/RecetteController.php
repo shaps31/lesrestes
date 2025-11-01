e4027b407ccfe9824f125a6f1866f7f21bd1e1ea
Shabadine <bah.shabadine@outlook.fr>
2025-11-01 12:04:39 +0100

    feat(Security): Sécurisation de RecetteController aux utilisateurs connectés (ROLE_USER)

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/recette')]
+#[IsGranted('ROLE_USER')]
 final class RecetteController extends AbstractController
 {
     #[Route(name: 'app_recette_index', methods: ['GET'])]

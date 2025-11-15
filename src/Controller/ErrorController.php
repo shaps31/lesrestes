116f30e074235c907a59a182828b25206c00f3dc
Shabadine <bah.shabadin@outlook.fr>
2025-11-15 13:59:34 +0100

    feat: Controller gestion erreurs personnalisees
    - Creation ErrorController pour gestion erreurs
    - Affichage status code et message
    - Support erreurs 404, 500 et generiques

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ErrorController extends AbstractController
+{
+    public function show(\Throwable $exception): Response
+    {
+        $statusCode = $exception instanceof HttpExceptionInterface 
+            ? $exception->getStatusCode() 
+            : 500;
+
+        return $this->render('bundles/TwigBundle/Exception/error' . $statusCode . '.html.twig', [
+            'status_code' => $statusCode,
+            'status_text' => $exception instanceof HttpExceptionInterface 
+                ? Response::$statusTexts[$statusCode] ?? 'Error' 
+                : 'Internal Server Error',
+        ], new Response('', $statusCode));
+    }
+
+    #[Route('/test-404', name: 'app_test_404')]
+    public function test404(): Response
+    {
+        throw $this->createNotFoundException('Page de test 404');
+    }
+}
